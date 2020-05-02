dc6347dbdc2753f58e799b8c7b37e8756d93ca43
SOFIAshyn <sophiya2petryshyn@gmail.com>
2020-05-02 20:35:50 +0300

    Updates

---
+++ precache-manifest.e08050bcbb949851d3b0bc1f95e85c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1e270c1a55efb51a2ea5371261abcf",
+    "url": "/react-my-app/index.html"
+  },
+  {
+    "revision": "d44c977f8676c53179d9",
+    "url": "/react-my-app/static/css/main.d49d0800.chunk.css"
+  },
+  {
+    "revision": "d5b67ca8ddba7bfed5e6",
+    "url": "/react-my-app/static/js/2.46e92fcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-my-app/static/js/2.46e92fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44c977f8676c53179d9",
+    "url": "/react-my-app/static/js/main.95c67125.chunk.js"
+  },
+  {
+    "revision": "3fce6030d069681bb0ed",
+    "url": "/react-my-app/static/js/runtime-main.8aa7da1c.js"
+  }
+]);
